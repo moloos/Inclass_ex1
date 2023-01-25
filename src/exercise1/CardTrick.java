9119b9b79bdb44a973c6029bbd3d6aa8f303d370
moloos <shirazmoloo@gmail.com>
2023-01-25 14:10:21 -0500

    I'm done

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -66,7 +66,7 @@ public class CardTrick {
      * replace this information with your own.
      * @author Paul Bonenfant Jan 2022
      */
-    
+    //I'm done
     private static void printInfo() {
     
         System.out.println("Congratulations, you guessed right!");
